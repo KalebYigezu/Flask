053dec703274ac3046a6b21c74354297dbdc1247
KalebYigezu <43226925+KalebYigezu@users.noreply.github.com>
2024-10-24 11:20:25 -0700

    Update app.py

--- app.py
+++ app.py
@@ -29,3 +29,16 @@ def hello(name):
     return f"<h1>Hello {name}<h1>"
 
 app.run()
+
+--------------------------------------------
+         Basics ++
+
+from flask import Flask, render_template
+app = Flask(__name__)
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+app.run()
+
+
