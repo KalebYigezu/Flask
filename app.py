491c88d7f9cb1f038344d5e512bc3b99df6ed45b
KalebYigezu <43226925+KalebYigezu@users.noreply.github.com>
2024-10-24 11:04:21 -0700

    Update app.py

--- app.py
+++ app.py
@@ -7,3 +7,25 @@ def index():
     return "Hello!"
 
 app.run()
+
+       Basics
+-------------------------------------------------
+       Basics + 
+
+from flask import Flask
+app = Flask(__name__)
+@app.route('/')
+def index():
+    return "Hello!"
+
+
+@app.route('/david')
+def david():
+    return "Hello David!"
+
+@app.route("/<name>")
+def hello(name):
+    name = name.capitalize()
+    return f"<h1>Hello {name}<h1>"
+
+app.run()
